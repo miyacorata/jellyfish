bced26f638b7fd6cf40277895c2a259cb0ec039e
K Miyano <miyano@miyacorata.net>
2020-06-01 13:30:59 +0900

    Add fallback route

---
+++ src/views/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="notfound">
+        <div class="basic-content" style="text-align: center">
+            <h2>Not Found</h2>
+            <p>Unfortunately, there is nothing here.</p>
+            <p></p>
+            <p>
+                <router-link to="/">Home</router-link> |
+                <a href="https://github.com/miyacorata/jellyfish" target="_blank" rel="noopener">Report to Admin</a>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped>
+
+</style>
