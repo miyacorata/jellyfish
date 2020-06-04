5d8305fa886a1aa11c2a0cbab130af999f93ce43
K Miyano <miyano@miyacorata.net>
2020-06-04 16:26:58 +0900

    WorkCard & .json

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -5,15 +5,25 @@
             <h3>{{ title }}</h3>
             <p>{{ description }}</p>
             <div class="tags">
-                <span v-for="tag in tags" v-bind:key="tag.value" :class="'tag '+tag.category">{{ tag.value }}</span>
+                <span v-for="tag in tags" :key="tag.value" :class="'tag '+tag.category">{{ tag.value }}</span>
+            </div>
+            <div class="links">
+                <a v-for="link in links" :key="link.name" :href="link.url" target="_blank" rel="noopener">{{ link.name }}</a>
+                <github-button v-if="github" :href="'https://github.com/'+github" data-size="large"
+                               data-show-count="true" :aria-label="'Star '+github+' on GitHub'">Star</github-button>
             </div>
         </div>
     </article>
 </template>
 
 <script>
+    import GithubButton from 'vue-github-button'
+
     export default {
         name: "WorkCard",
+        components: {
+            GithubButton
+        },
         props: {
             image: {
                 type: String,
@@ -23,11 +33,21 @@
                 type: String,
                 required: true
             },
+            github: {
+                type: String,
+                default: null
+            },
             description: {
                 type: String,
                 required: true
             },
             tags: {
+                type: Array,
+                default: () => {
+                    return []
+                }
+            },
+            links: {
                 type: Array,
                 default: () => {
                     return []
@@ -41,11 +61,15 @@
     .work-card {
         display: flex;
         align-items: flex-start;
+        margin-bottom: 15px;
     }
     .work-card > img:first-child{
         width: 250px;
         box-sizing: border-box;
         margin-right: 20px;
+    }
+    .work-card > div {
+        width: 100%;
     }
     .work-card > div > h3:first-child{
         margin: 0 0 0.3em;
@@ -53,12 +77,13 @@
     }
     .work-card > div > p{
         margin: 1em 0;
+        white-space: pre-wrap;
     }
 
     .tag {
         display: inline-block;
         margin-right: 10px;
-        padding: 0 5px;
+        padding: 0 8px 0 6px;
         border-radius: 2px;
         border-left: solid 5px blue;
         background: floralwhite;
@@ -70,5 +95,41 @@
     }
     .tag.tech {
         border-left-color: deepskyblue;
+    }
+
+    .work-card .links {
+        text-align: right;
+        margin: 10px 0;
+    }
+    .work-card .links > a {
+        display: inline-block;
+        vertical-align: top;
+        margin-right: 10px;
+        padding: 2px 8px;
+        font-size: 16px;
+        text-decoration: none;
+        border: solid 1px #61bf99;
+        border-radius: 3px;
+        transition: background-color .3s, color .3s;
+    }
+    .work-card .links > a:hover {
+        background: #61bf99;
+        color: #151515;
+    }
+    .work-card .links > a:before {
+        content: '>';
+        font-weight: bold;
+        margin-right: 3px;
+    }
+    .work-card .links > a:hover:before {
+        animation: blinker ease 1s infinite;
+    }
+    .v-leave-active .work-card .links > span {
+        opacity: 0;
+    }
+    @keyframes blinker {
+        0%   { opacity: 1 }
+        50%  { opacity: 0.1 }
+        100% { opacity: 1 }
     }
 </style>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -5,23 +5,8 @@
                      subline="これまでの制作物"/>
         <div class="basic-content">
             <h2>Websites</h2>
-            <WorkCard image="https://files.miyacorata.net/images/jellyfish/mlp.jpg"
-                      title="MillionLivePortal"
-                      description="アイドルマスターミリオンライブ！の非公式DB＆ポータルサイト"
-                      :tags="[
-                          {
-                              category: 'content',
-                              value: 'THE IDOLM@STER'
-                          },
-                          {
-                              category: 'tech',
-                              value: 'PHP'
-                          },
-                          {
-                              category: 'tech',
-                              value: 'Laravel'
-                          }
-                      ]"/>
+            <WorkCard v-for="work in works.websites" :key="work.name" :image="work.image" :title="work.name"
+                      :description="work.description" :tags="work.tags" :links="work.links" :github="work.github"/>
         </div>
     </div>
 </template>
@@ -29,9 +14,16 @@
 <script>
     import HeadPicture from "../components/HeadPicture";
     import WorkCard from "../components/WorkCard";
+    import works from "../datas/works.json";
+
     export default {
         name: "Works",
-        components: {WorkCard, HeadPicture}
+        components: {WorkCard, HeadPicture},
+        data: ()=>{
+            return {
+                works: works
+            }
+        }
     }
 </script>
 
